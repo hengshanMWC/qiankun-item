46da2c58e8c36acd82323d71fc42e25c78e593c6
mawencan <1819293933@qq.com>
2021-12-11 12:36:47 +0800

    feat: angular打通

--- qiankun-angular/src/main.ts
+++ qiankun-angular/src/main.ts
@@ -1,16 +1,38 @@
-import './public-path';
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, NgModuleRef } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 import 'qiankun';
+import changePublicPath from './public-path'; // 暴露public-path文件
 
+changePublicPath();
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+let app: void | NgModuleRef<AppModule>;
+async function render() {
+  app = await platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
+}
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
+export async function bootstrap(props: QiankunRenderProps) {
+  console.log('[angular] angular app bootstraped', props);
+}
+
+export async function mount(props: QiankunRenderProps) {
+  console.log('[angular] props from main framework', props);
+  render();
+}
+
+export async function unmount(props: QiankunRenderProps) {
+  console.log('[angular] angular app unmount', props);
+  // @ts-ignore
+  app.destroy();
+}
--- qiankun-vue2/src/main.js
+++ qiankun-vue2/src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import "zone.js"
 import { registerMicroApps, start } from 'qiankun'
 
 Vue.config.productionTip = false
@@ -20,6 +22,12 @@ registerMicroApps([
     entry: '//localhost:8002', // 微应用入口
     container: '#app', // 微应用的容器
     activeRule: '/react' // url匹配微应用规则
+  },
+  {
+    name: 'qiankun-angular', // 微应用的名称
+    entry: '//localhost:8003', // 微应用入口
+    container: '#app', // 微应用的容器
+    activeRule: '/angular' // url匹配微应用规则
   }
 ])
 start()
